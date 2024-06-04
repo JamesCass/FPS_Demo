aa1916b0cf8aeba78d5a21d40e9e62c41e5ebabc
JamesCass <136556196+JamesCass@users.noreply.github.com>
2024-06-04 20:49:37 +1000

    Fixed particles when breaking blocks

--- Scripts/DestructableGridMap.gd
+++ Scripts/DestructableGridMap.gd
@@ -7,8 +7,10 @@ var destroyed_block = load("res://Scenes/destroyed_block.tscn")
 func destroy_tile(collision_point):
 	var map_coordinate = local_to_map(collision_point)
 	set_cell_item(map_coordinate, -1)
+	
 	instance = destroyed_block.instantiate()
 	instance.position = map_to_local(map_coordinate)
+	instance.emitting = true
 	add_child(instance)
 
 func _process(delta):
